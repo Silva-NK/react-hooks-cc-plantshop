03a2d8dbc695db1f16f886cab9d9e7bdf1189cee
Silva-NK <kiagonsuzanne@gmail.com>
2025-05-01 23:13:09 +0300

    Adds plant view, enable addition of new plant, search and changing sold out status.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(response => response.json())
+    .then(data => {
+      setPlants(data);
+    })
+    .catch (err => {
+      console.error("Error fetching plants:", err);
+    })
+  }, []);
+
+  const handleAddPlant = (newPlant) => {
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/JSON"
+      },
+      body: JSON.stringify(newPlant)
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      setPlants((prevPlants) => [...prevPlants, data]);
+    });
+  };
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants} onAddPlant={handleAddPlant}/>
     </div>
   );
 }
--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({image, name, price}) {
+  const [soldOut, setSoldOut] = useState(false);
+
+  function handleToggleSoldOut() {
+    setSoldOut((prev) => !prev);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {soldOut ? (
+        <button onClick={handleToggleSoldOut} >Out of Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button className="primary" onClick={handleToggleSoldOut} >In Stock</button>
       )}
     </li>
   );
